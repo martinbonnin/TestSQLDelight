fc78e8408abb8fe445ced8ace9c138a96eb2c8bf
Martin Bonnin <martin@mbonnin.net>
2019-08-06 12:53:38 +0200

    make an actual query

--- app/src/main/java/net/mbonnin/testsqldelight/MainActivity.kt
+++ app/src/main/java/net/mbonnin/testsqldelight/MainActivity.kt
@@ -2,11 +2,30 @@ package net.mbonnin.testsqldelight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.squareup.sqldelight.EnumColumnAdapter
+import com.squareup.sqldelight.Query
+import com.squareup.sqldelight.android.AndroidSqliteDriver
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Database(driver = AndroidSqliteDriver(Database.Schema, this, "test"),
+            hockeyPlayerDb2Adapter = HockeyPlayerDb2.Adapter(
+                positionAdapter = EnumColumnAdapter()
+            ),
+            hockeyPlayerDbAdapter = HockeyPlayerDb.Adapter(
+                positionAdapter = EnumColumnAdapter()
+            )
+        )
+
+        db.hockeyQueries.selectAll().addListener(object: Query.Listener {
+            override fun queryResultsChanged() {
+                Log.d("DB", "query changed")
+            }
+        })
     }
 }
